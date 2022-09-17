cbf4589f97b9413389bd0d29e3ee99f07ff6d580
vgeorge-fi <v.george@fi.com>
2022-09-17 14:28:07 -0700

    Test commits

--- dist/index.js
+++ dist/index.js
@@ -2808,8 +2808,8 @@ try {
     const text = core.getInput('who-to-greet');
     const reverse = text ? text.split('').reverse().join('') : text;
 
-    console.log(`text ${text}`);
-    console.log(`reverse ${reverse}`);
+    console.log(`text: ${text}`);
+    console.log(`reverse: ${reverse}`);
 
     //core.setOutput("text", text);
     core.setOutput("reverse", reverse);
--- index.js
+++ index.js
@@ -4,8 +4,8 @@ try {
     const text = core.getInput('who-to-greet');
     const reverse = text ? text.split('').reverse().join('') : text;
 
-    console.log(`text ${text}`);
-    console.log(`reverse ${reverse}`);
+    console.log(`text: ${text}`);
+    console.log(`reverse: ${reverse}`);
 
     //core.setOutput("text", text);
     core.setOutput("reverse", reverse);

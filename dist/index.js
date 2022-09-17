bfe1d84f0736277add811d4c271d57c1d6c61f52
vgeorge-fi <v.george@fi.com>
2022-09-17 13:44:41 -0700

    Modified index.js script.

--- dist/index.js
+++ dist/index.js
@@ -9673,9 +9673,6 @@ try {
     console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }

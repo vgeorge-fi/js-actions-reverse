60e5121ca2a4265a42f3bf48b4425e28c5987797
vgeorge-fi <v.george@fi.com>
2022-09-17 14:46:30 -0700

    Test commits

--- dist/index.js
+++ dist/index.js
@@ -2808,9 +2808,9 @@ try {
     const text = core.getInput('text');
     const reverse = text ? text.split('').reverse().join('') : text;
 
+    console.log('\n');
     console.log(`text: ${text}`);
     console.log(`reverse: ${reverse}`);
-
     core.setOutput("text", text);
     core.setOutput("reverse", reverse);
 } catch (error) {
